fdc7d907fe000dbf9e73679b313191a8219e1cfa
aoi1 <tkhsaoi88@gmail.com>
2019-04-09 22:06:53 +0900

    added public directory for illust.png

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
+var fs = require('fs');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static('public'));
+app.get('/', (req, res) =>
+  fs.readFile('./index.html', 'UTF-8', function(err, data){
+          res.writeHead(200, {'Content-Type': 'text/html'});
+          res.end(data);
+  })
+)
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
