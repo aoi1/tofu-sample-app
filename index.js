e9eeeecf46d56724a0d8cc01a5c802225d618743
aoi1 <tkhsaoi88@gmail.com>
2019-04-11 20:18:43 +0900

    moved illust directory

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
 var fs = require('fs');
+var path = require('path');
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) =>
   fs.readFile('./index.html', 'UTF-8', function(err, data){
           res.writeHead(200, {'Content-Type': 'text/html'});
